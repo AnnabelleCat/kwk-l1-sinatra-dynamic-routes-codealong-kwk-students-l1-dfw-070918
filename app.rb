86e00dc284dda3cee9875f8954218511ee59cfd0
AnnabelleCat <pikainka@gmail.com>
2018-07-19 18:57:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,18 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
+  
+  get '/food_form' do
+    erb :food_form 
   end
-
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
-
-  # Code your final two routes here:
-
+  
+  post '/food'  do
+    "Let's play!"
+    end
+   
 end
